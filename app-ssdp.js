72506331551885d1ba0bdf86247283b4d8d314de
Caspar van der Linden <c.vanderlinden@rubicon.nl>
2025-12-20 18:02:05 +0100

    revisit SSDP

--- app-ssdp.js
+++ app-ssdp.js
@@ -1,15 +1,19 @@
 var Client = require('node-ssdp').Client
-    , client = new Client({explicitSocketBind: true});
+    , client = new Client({ explicitSocketBind: true });
+
+
+let counter = 0;
 
 client.on('response', function (headers, statusCode, rinfo) {
-    console.log('Got a response to an m-search.');
+    counter++;
+    console.log(counter, 'Response to an m-search.', rinfo.address, headers.ST);
+    console.log("  ", headers.LOCATION);
 });
 
 // search for a service type
 // client.search('urn:schemas-upnp-org:service:ContentDirectory:1');
 
 // Or get a list of all services on the network
-
 client.search('ssdp:all');
 
 // Wait a bit for results to come in...
